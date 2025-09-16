9d924be36909777bd92a1621b22bd6f1d1e212c2
idetanaoya <118895979+regal-tiger-paw@users.noreply.github.com>
2025-09-17 06:22:32 +0900

    追加_APIの実装

--- src/main.js
+++ src/main.js
@@ -1,24 +1,53 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import axios from "axios";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.querySelector("form").addEventListener("submit", (e) => {
+  // ページのリロードを防ぐ（submitイベントのデフォルト動作をキャンセル）
+  e.preventDefault();
 
-setupCounter(document.querySelector('#counter'))
+  // inputの値を取得
+  const input = e.target.elements.pokeName.value.trim(); // trim()で前後の空白を削除
+  // 英語でなければアラートを出す
+  if (!/^[a-zA-Z]+$/.test(input)) {
+    alert("英語のポケモン名を入力してください。");
+    return;
+  }
+
+  // axiosのインスタンスを作成
+  const instance = axios.create({
+    baseURL: "https://pokeapi.co/api/v2/",
+  });
+
+  // APIを叩く関数を定義
+  const fetchData = async (pokeName) => {
+    try {
+      const res = await instance.get(`/pokemon/${pokeName.toLowerCase()}`);
+      return res.data;
+    } catch (err) {
+      console.error(err);
+      alert("ポケモンが見つかりませんでした。");
+    }
+  };
+
+  const result = fetchData(input);
+
+  result.then((data) => {
+    const name = data.name; // ポケモンの名前
+    const height = data.height * 10; // デシメートルをセンチメートルに変換
+    const weight = data.weight * 0.1; // ヘクトグラムをキログラムに変換
+    const imgUrl = data.sprites.front_default; // ポケモンの画像URL
+    const img = document.getElementById("js-img"); // 画像表示用のimg要素
+
+    // ポケモン画像URLが取得できた場合
+    if (imgUrl) {
+      img.src = imgUrl;
+      img.style.display = "";
+    } else {
+      img.src = "";
+      img.style.display = "none";
+    }
+    // 結果表示
+    document.getElementById(
+      "js-result"
+    ).innerText = `名前: ${name}\n高さ: ${height}cm\n重さ: ${weight}kg`;
+  });
+});
